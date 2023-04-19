d60495d44660323477bf72bb69e741db5cfba303
Brad Stell <hi@bradleystell.com>
2023-04-19 17:53:33 -0400

    removing files left over from project generator

--- utils.ts
+++
@@ -1,7 +0,0 @@
-export type Nullish = null | undefined
-
-export type Maybe<T> = T | Nullish
-
-export const isNotNil = <T>(val: T): val is Exclude<T, Nullish> => {
-  return val !== undefined && val !== null
-}
